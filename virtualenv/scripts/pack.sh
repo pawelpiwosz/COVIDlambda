f9f12f5036cc9534ab234eef287f4038840939e7
Pawel Piwosz <pawel@pawelpiwosz.net>
2020-04-02 01:26:10 +0200

    test

--- virtualenv/scripts/pack.sh
+++ virtualenv/scripts/pack.sh
@@ -3,7 +3,8 @@ mkdir artifacts
 ls -al
 cd covid/lib/python3.6/site-packages/
 pwd
-zip -g -r ./../../artifacts/covidlambda.zip . 
+ls ../../../../
+zip -g -r ./../../../../artifacts/covidlambda.zip . 
 cd
 ls -al artifacts
 cd py
