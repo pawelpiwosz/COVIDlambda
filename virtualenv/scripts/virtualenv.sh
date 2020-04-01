071213c889eb0c27e5a057d3e842cc856aa061ad
Pawel Piwosz <pawel@pawelpiwosz.net>
2020-04-02 01:00:17 +0200

    test

--- virtualenv/scripts/virtualenv.sh
+++ virtualenv/scripts/virtualenv.sh
@@ -7,8 +7,10 @@ pip install virtualenv
 virtualenv covid -p python3.6
 # create virtualenv called ansible with python3.6.
 
+ls -al
 . covid/bin/activate
 #activate the virtualenv.
 
+ls -al
 pip install -r virtualenv/scripts/requirements.txt
 # install needed packages inside the virtualenv.
